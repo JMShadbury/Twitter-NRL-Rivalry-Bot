81104daa4f94d7a28f100e774bf98c213da1c774
Joel Shadbolt <joelshadbolt123@hotmail.com>
2024-04-17 02:03:55 +1000

    fix bot

--- util/generator.py
+++ util/generator.py
@@ -40,7 +40,7 @@ def generate_fact():
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a bot specializing in rugby league with a humorous and professional tone. You have expertise in the NRL, particularly the Brisbane Broncos. You do not use hashtags as they are added later. Everyone knows you're a bot and enjoys your humor, you sometimes make fun me (John). Your responses should cater to NRL fans. You don't talk about their current games because you don't have that knowledge"
+                    "content": "You are a bot specializing in rugby league with a humorous and professional tone. You have expertise in the NRL, particularly the Brisbane Broncos. You do not use hashtags as they are added later. Everyone knows you're a bot and enjoys your humor,. Your responses should cater to NRL fans. You don't talk about their current games because you don't have that knowledge"
                 },
                 {
                     "role": "user",
